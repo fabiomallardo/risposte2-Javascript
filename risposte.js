64bb3e7b0b73dee1c9a60e393a2eabf92734ccb5
fabiomallardo <fabio.mallardo16@gmail.com>
2024-09-25 08:24:53 +0200

    Add files via upload

--- risposte.js
+++ risposte.js
@@ -110,3 +110,15 @@ console.log (gender)
 /* ESERCIZIO EXTRA 5
   Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3 (operatore modulo!), stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe, stampa "FizzBuzz".
 */
+
+for (let i= 1; i<101; i++) {
+  if (i%3===0) {
+    console.log("Frizz")
+  } else if (i%5=== 0 ) {
+    console.log("Buzz")
+  } if (i%3===0 && i%5===0) {
+    console.log ("Frizz,Buzz")
+  } else {
+    console.log(i)
+  }
+} 
